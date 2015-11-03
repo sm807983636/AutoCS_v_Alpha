c7be273fe2980b962cb6b1343cea169b2f6402e1
heronsbill <981141175@qq.com>
2015-11-03 23:17:27 +0800

    first UI git

---
+++ UI/教学办-账号.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+
+function addFav(title){
+	var url=location.href;}
+	if(window.sidebar){
+		window.sidebar.addPane(title,url,"");}
+    if(document.all){
+			window.external.AddFavorite(url,title);
+			}
+			else if(window.opera&&window.print){
+				return true;}
